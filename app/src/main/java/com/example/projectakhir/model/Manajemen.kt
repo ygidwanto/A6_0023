a66dff08d298c2ede03f61515cd26ee77f85681d
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-22 22:12:20 +0700

    membuat interface acara

--- app/src/main/java/com/example/projectakhir/model/Manajemen.kt
+++ app/src/main/java/com/example/projectakhir/model/Manajemen.kt
@@ -2,14 +2,14 @@ package com.example.projectakhir.model
 
 @Serializable
 data class Acara(
-    val id: Int,  // Primary Key
+    val id: Int,
     val namaAcara: String,
     val deskripsiAcara: String,
     val tanggalMulai: String,
     val tanggalBerakhir: String,
-    val idLokasi: Int,  // Foreign Key ke tabel Lokasi
-    val idKlien: Int,   // Foreign Key ke tabel Klien
-    val statusAcara: String // (Direncanakan, Berlangsung, Selesai)
+    val idLokasi: Int,
+    val idKlien: Int,
+    val statusAcara: String
 )
 
 @Serializable
---
+++ app/src/main/java/com/example/projectakhir/service_api/AcaraService.kt
@@ -0,0 +1,24 @@
+package com.example.projectakhir.service_api
+
+import com.example.projectakhir.model.Acara
+
+interface AcaraService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+    @POST("insertacara.php")
+    suspend fun insertAcara(@Body acara: Acara)
+
+    @GET("bacaacara.php")
+    suspend fun getAllAcara(): List<Acara>
+
+    @GET("baca1acara.php/{id}")
+    suspend fun getAcaraById(@Path("id") id: Int): Acara
+
+    @PUT("editacara.php/{id}")
+    suspend fun updateAcara(@Path("id") id: Int, @Body acara: Acara)
+
+    @DELETE("deleteacara.php/{id}")
+    suspend fun deleteAcara(@Path("id") id: Int): Response<Void>
+}
