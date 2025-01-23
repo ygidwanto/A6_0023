333abea5277d3f5ef9e859dca653c3cbdd3e6871
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-23 20:17:27 +0700

    membuat class network  Repositori

--- app/src/main/java/com/example/projectakhir/repository/LokasiRepository.kt
+++ app/src/main/java/com/example/projectakhir/repository/LokasiRepository.kt
@@ -1,6 +1,7 @@
 package com.example.projectakhir.repository
 
 import com.example.projectakhir.model.Lokasi
+import com.example.projectakhir.service_api.LokasiService
 
 interface LokasiRepository {
     suspend fun insertLokasi(lokasi: Lokasi)
@@ -8,4 +9,35 @@ interface LokasiRepository {
     suspend fun updateLokasi(id: Int, lokasi: Lokasi)
     suspend fun deleteLokasi(id: Int)
     suspend fun getLokasiById(id: Int): Lokasi
+}
+class NetworkLokasiRepository(
+    private val lokasiApiService: LokasiService
+) : LokasiRepository {
+    override suspend fun insertLokasi(lokasi: Lokasi) {
+        lokasiApiService.insertLokasi(lokasi)
+    }
+
+    override suspend fun updateLokasi(id: Int, lokasi: Lokasi) {
+        lokasiApiService.updateLokasi(id, lokasi)
+    }
+
+    override suspend fun deleteLokasi(id: Int) {
+        try {
+            val response = lokasiApiService.deleteLokasi(id)
+            if (!response.isSuccessful) {
+                throw IOException("Gagal menghapus Lokasi. HTTP Status code: ${response.code()}")
+            } else {
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getAllLokasi(): List<Lokasi> =
+        lokasiApiService.getAllLokasi()
+
+    override suspend fun getLokasiById(id: Int): Lokasi {
+        return lokasiApiService.getLokasiById(id)
+    }
 }
