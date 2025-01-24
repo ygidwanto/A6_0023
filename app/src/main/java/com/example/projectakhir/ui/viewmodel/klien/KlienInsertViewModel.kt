5304bd3c73b5418c8991558e1f67007381bf38c2
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-01-24 13:45:18 +0700

    membuat inser viewmodelklien

--- app/src/main/java/com/example/projectakhir/ui/viewmodel/klien/KlienInsertViewModel.kt
+++ app/src/main/java/com/example/projectakhir/ui/viewmodel/klien/KlienInsertViewModel.kt
@@ -1,2 +1,59 @@
 package com.example.projectakhir.ui.viewmodel.klien
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectakhir.repository.KlienRepository
+import com.example.projectakhir.model.Klien
+import kotlinx.coroutines.launch
+
+@kotlinx.serialization.InternalSerializationApi
+class InsertViewModel(private val klien: KlienRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKlienState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKlien() {
+        viewModelScope.launch {
+            try {
+                klien.insertKlien(uiState.insertUiEvent.toKlien())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val namaKlien: String = "",
+    val kontakKlien: String = ""
+)
+
+@kotlinx.serialization.InternalSerializationApi
+fun InsertUiEvent.toKlien(): Klien = Klien(
+    id = id,
+    namaKlien = namaKlien,
+    kontakKlien = kontakKlien
+)
+
+@kotlinx.serialization.InternalSerializationApi
+fun Klien.toUiStateKlien(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+@kotlinx.serialization.InternalSerializationApi
+fun Klien.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    namaKlien = namaKlien,
+    kontakKlien = kontakKlien
+)
